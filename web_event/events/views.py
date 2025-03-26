9f2be36070fa19a833a627b3883716d7c2e04e8b
David Brown <dgb_online@icloud.com>
2025-03-26 18:04:28 -0400

    initial commit

---
+++ web_event/events/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "John"
+    month = month.title()
+    #convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    #create calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    #get current year
+    now = datetime.now()
+    current_year = now.year
+    #get current time
+    time = now.strftime('%I:%M %p')
+
+    return render(request, 'events/home.html', {"name": name,
+                                         "year": year,
+                                         "month": month,
+                                         "month_number": month_number,
+                                         "cal": cal,
+                                         "current_year": current_year,
+                                         "time": time,
+                                         })
+                                         
+                                    
+                                        
+
