9646ceb7ad44df65ff77878141eac99e229b6d8f
David Brown <dgb_online@icloud.com>
2025-03-27 06:47:40 -0400

    Modify Admin. Part 7

--- web_event/events/models.py
+++ web_event/events/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
-    zip_code = models.CharField('Zip Code', max_length=15)
-    phone = models.CharField('Contact Phone', max_length=25)
-    web = models.URLField('Website Address')
-    email_address= models.EmailField('Email Address')
+    zip_code = models.CharField('Zip Code', max_length=15, blank=True)
+    phone = models.CharField('Contact Phone', max_length=25, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address= models.EmailField('Email Address', blank=True)
 
 
     def __str__(self):
@@ -26,7 +27,7 @@ class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
 
