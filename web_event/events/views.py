17c9a69877859b7298a76fb75648aff3b2aac252
David Brown <dgb_online@icloud.com>
2025-03-27 10:50:56 -0400

    Add db Forms, Part 7

--- web_event/events/views.py
+++ web_event/events/views.py
@@ -3,6 +3,8 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
+from .forms import VenueForm
+from django.http import HttpResponseRedirect
 
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
@@ -33,3 +35,17 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
 def all_events(request):
     event_list = Event.objects.all()
     return render(request, 'events/event_list.html', {"event_list":event_list})
+
+
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True') # ?variable
+    else:
+         form = VenueForm
+         if 'submitted' in request.GET:
+             submitted = True
+    return render(request, 'events/add_venue.html', {'form':form, 'submitted':submitted})
