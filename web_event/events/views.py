867e3c6434e91773008c6987ad45ea5c2fb3d1a4
David Brown <dgb_online@icloud.com>
2025-03-27 14:30:03 -0400

    Query db for Venues, Part 8

--- web_event/events/views.py
+++ web_event/events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
-from .models import Event
+from .models import Event, Venue
 from .forms import VenueForm
 from django.http import HttpResponseRedirect
 
@@ -34,7 +34,7 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
                                     
 def all_events(request):
     event_list = Event.objects.all()
-    return render(request, 'events/event_list.html', {"event_list":event_list})
+    return render(request, 'events/event_list.html', {'event_list':event_list})
 
 
 def add_venue(request):
@@ -49,3 +49,13 @@ def add_venue(request):
          if 'submitted' in request.GET:
              submitted = True
     return render(request, 'events/add_venue.html', {'form':form, 'submitted':submitted})
+
+
+def list_venues(request):
+    venue_list = Venue.objects.all()
+    return render(request, 'events/venue.html', {'venue_list':venue_list})
+
+
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, 'events/show_venue.html', {'venue':venue})
